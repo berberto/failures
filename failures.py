406c539d16103ee8ccedac45a96c1393eb832b77
berberto <alberto.pzt@gmail.com>
2022-10-25 14:33:25 +0100

    roman's code -- N and drop_p as inputs, changed slurm script and pars

--- failures.py
+++ failures.py
@@ -1,88 +1,26 @@
 from __future__ import print_function
 import argparse
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
+from torch.optim.lr_scheduler import StepLR, CosineAnnealingLR
 from matplotlib import pyplot as plt
 # import seaborn as sns
 # sns.set_style('whitegrid', {'font_scale': 2})
 import functools
-import sys
-import os
 
 
-class WeightDropLinear(nn.Linear):
-    def __init__(self, *args, weight_dropout=0.0, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.weight_dropout = weight_dropout
-
-    def forward(self, x):
-        if not self.training:
-            return F.linear(x, self.weight, self.bias)
-        new_weight = (torch.rand((x.shape[0], *self.weight.shape), device=x.device) > self.weight_dropout) * self.weight[None, :, :]
-        output = torch.bmm(new_weight, x[:, :, None])[:, :, 0] / (1 - self.weight_dropout)
-        if self.bias is None:
-            return output
-        return output + self.bias
-
-
-class Net(nn.Module):
-    '''
-    Network class
-    '''
-    def __init__(self, N, layer_type=nn.Linear, scaling="sqrt", lr=5e-2, verbose=False, **kwargs):
-        super(Net, self).__init__()
-
-        self.fc1 = layer_type(28 ** 2, N, bias=False, **kwargs)
-        # self.fc1 = layer_type(28 ** 2, N, bias=False) # additional hidden layer
-        self.fc2 = layer_type(N, 10, bias=False, **kwargs).requires_grad_(False) # frozen weights
-
-        if verbose:
-          for name, pars in self.named_parameters():
-              print("=====")
-              print(name)
-              print("-----")
-              f_in = pars.data.size()[1]
-              print(f"input size = {f_in}")
-
-        if scaling == "lin":
-            # initialisation of the weights -- N(1/n, 1/n)
-            for name, pars in self.named_parameters():
-                f_in = 1.*pars.data.size()[1]
-                pars.data.normal_(1./f_in, 1./f_in)
-        elif scaling == "sqrt":
-            # initialisation of the weights -- N(0, 1/sqrt(n))
-            for name, pars in self.named_parameters():
-                f_in = 1.*pars.data.size()[1]
-                pars.data.normal_(0., 2./np.sqrt(f_in))
-        else:
-            raise ValueError(f"Invalid scaling option '{scaling}'\nChoose either 'sqrt' or 'lin'")
-
-        # SGD over the first layer 
-        self.optimizer = optim.SGD(self.parameters(), lr=lr, weight_decay=0.)
-
-    def forward(self, x):
-        x = torch.flatten(x, 1)
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.fc2(x)
-        x = F.softmax(x, dim=1)
-        return x
-
-def train(model, device, train_loader, epoch, log_interval=100):
+def train(model, device, train_loader, optimizer, epoch, log_interval=100):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
-        model.optimizer.zero_grad()
+        optimizer.zero_grad()
         output = model(data)
-        # "sum" or "mean" refers to the sum/average over both batch AND dimension indices
-        # e.g. for a batch size of 64 and 10 classes, it is a sum/average of 640 numbers
-        loss = F.mse_loss(output, F.one_hot(target, num_classes=10).float(), reduction="sum")
+        loss = F.mse_loss(output, F.one_hot(target, num_classes=10).float())# F.nll_loss(output, target)
         loss.backward()
-        model.optimizer.step()
+        optimizer.step()
         if batch_idx % log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
@@ -107,69 +45,84 @@ def test(model, device, test_loader):
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
     with torch.no_grad():
-        weights = list(model.parameters())
-        model_norm = (torch.linalg.norm(weights[0]).item(), torch.linalg.norm(weights[1]).item())
+        model_norm = (torch.linalg.norm(model.fc1.weight).item(), torch.linalg.norm(model.fc2.weight).item())
     return 100. * correct / len(test_loader.dataset), model_norm[0], model_norm[1]
+
+
+class LinearExampleDropout(nn.Linear):
+    def __init__(self, in_features, out_features, bias=True, device=None, dtype=None, drop_p=0.0):
+        super().__init__(in_features, out_features, bias, device, dtype)
+        self.drop_p = drop_p
+
+    def forward(self, input):
+        if not self.training:
+            return F.linear(input, self.weight, self.bias)
+        new_weight = (torch.rand((input.shape[0], *self.weight.shape), device=input.device) > self.drop_p) * self.weight[None, :, :]
+        output = torch.bmm(new_weight, input[:, :, None])[:, :, 0] / (1 - self.drop_p)
+        if self.bias is None:
+            return output
+        return output + self.bias
+
+class Net(nn.Module):
+    def __init__(self, N, layer_type=nn.Linear):
+        super(Net, self).__init__()
+        self.fc1 = layer_type(28 ** 2, N)
+        self.fc2 = layer_type(N, 10)
+
+        with torch.no_grad():
+            nn.init.kaiming_normal_(self.fc1.weight, mode='fan_in', nonlinearity='relu')
+            nn.init.kaiming_normal_(self.fc2.weight, mode='fan_in', nonlinearity='relu')
+
+    def forward(self, x):
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        # output = F.log_softmax(x, dim=1)
+        return x#output
+
+
+def plot_weights_histograms (model, out_dir="."):
+    # histogram of initial parameters
+    weights = list(model.parameters())
+    weights_1 = weights[0].data.detach().cpu().numpy(); print(f"weights_1: {weights_1.shape}"); print(f"mean_1 = {np.mean(weights_1):.5f}, std_1 = {np.std(weights_1):.5f}, ")
+    weights_2 = weights[1].data.detach().cpu().numpy(); print(f"weights_2: {weights_2.shape}"); print(f"mean_2 = {np.mean(weights_2):.5f}, std_2 = {np.std(weights_2):.5f}, ")
+    plt.figure(figsize=(10, 5))
+    plt.hist(weights_1.ravel(), density=True, bins="sqrt", alpha=.3, label="w1")
+    plt.hist(weights_2.ravel(), density=True, bins="sqrt", alpha=.3, label="w2")
+    plt.axvline(0.,c="k")
+    plt.legend()
+    plt.savefig(f"{out_dir}/{model.__name__}_init_weights.svg", bbox_inches="tight")
+    np.save(f"{out_dir}/{model.__name__}_init_weights_1.npy", weights_1)
+    np.save(f"{out_dir}/{model.__name__}_init_weights_2.npy", weights_2)
 
 
 if __name__ == "__main__":
 
+    import sys
+    import os
+    import numpy as np
+
+    # ==================================================
+    #   SETUP PARAMETERS
 
     # get parameters as inputs
-    scaling = str(sys.argv[1])
-    ltype = str(sys.argv[2])
-    N = int(sys.argv[3])
-    dropout = float(sys.argv[4])
+    N = int(sys.argv[1])
+    drop_p = float(sys.argv[2])
 
     # set (and create) output directory
-    out_dir = "outputs/"
-    out_dir += f"scaling:{scaling}"
-    out_dir += f"_N:{N:04d}"
-    out_dir += f"_ltype:{ltype}"
-    if ltype == "drop":
-        out_dir += f"_dropout:{dropout:.2f}"
+    out_dir = "outputs_RP/"
+    out_dir += f"N:{N:04d}"
+    out_dir += f"_dropout:{drop_p:.2f}"
     os.makedirs(out_dir, exist_ok=True)
-
-    # define network options
-    layers = {"full": nn.Linear, "drop": WeightDropLinear}
-    net_kwargs = {}
-    net_kwargs["layer_type"] = layers[ltype]
-    net_kwargs["scaling"] = scaling
-    if ltype == "drop":
-        net_kwargs["weight_dropout"] = dropout
 
     # find device
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     print(f"device = {device}")
 
-    from pprint import pprint
-    pprint(net_kwargs)
-
-    # define network model (and put on available device)
-    model = Net(N, **net_kwargs).to(device)
-
-    print(model)
-    print("")
-
-    # histogram of initial parameters
-    weights = list(model.parameters())
-    weights_1 = weights[0].data.detach().cpu().numpy(); print(f"weights_1: {weights_1.shape}"); print(f"mean_1 = {np.mean(weights_1):.5f}, std_1 = {np.std(weights_1):.5f}, ")
-    weights_2 = weights[1].data.detach().cpu().numpy(); print(f"weights_2: {weights_2.shape}"); print(f"mean_2 = {np.mean(weights_2):.5f}, std_2 = {np.std(weights_2):.5f}, ")
-    fig, ax = plt.subplots()
-    ax.hist(weights_1.ravel(), density=True, bins="sqrt", alpha=.3, label="w1")
-    ax.hist(weights_2.ravel(), density=True, bins="sqrt", alpha=.3, label="w2")
-    ax.axvline(0.,c="k")
-    ax.legend()
-    fig.savefig(f"{out_dir}/init_weights.svg", bbox_inches="tight")
-    np.save(f"{out_dir}/init_weights_1.npy", weights_1)
-    np.save(f"{out_dir}/init_weights_2.npy", weights_2)
-    plt.close(fig)
-
     # ==================================================
-    n_epochs = 100
+    #   SETUP TRAINING
     batch_size = 256
-    lr = 5e-2
-    wd = 1e-4
     train_kwargs = {'batch_size': batch_size}
     test_kwargs = {'batch_size': batch_size}
     use_cuda = True
@@ -189,36 +142,88 @@ if __name__ == "__main__":
     train_loader = torch.utils.data.DataLoader(dataset1,**train_kwargs)
     test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
 
-
     # ==================================================
+    #   TRAINING WITHOUT DROPOUT
+    lr = 5e-2
+    wd = 1e-4
+    n_epochs = 100
 
-    # train and monitor performance and weight norm 
     test_acc = []
     model_norm1 = []
     model_norm2 = []
+
+    model = Net(N).to(device)
+    optimizer = optim.SGD(model.parameters(), lr=lr, weight_decay=wd)
+    scheduler = CosineAnnealingLR(optimizer, n_epochs)
+
+    print(model)
+
     for epoch in range(1, n_epochs + 1):
-        train(model, device, train_loader, epoch, log_interval=1000)
+        train(model, device, train_loader, optimizer, epoch, log_interval=1000)
         acc, weight_norm1, weight_norm2 = test(model, device, test_loader)
         test_acc.append(acc)
         model_norm1.append(weight_norm1)
         model_norm2.append(weight_norm2)
-    np.save(f"{out_dir}/norm_weights_1.npy", weight_norm1)
-    np.save(f"{out_dir}/norm_weights_2.npy", weight_norm2)
-
+        scheduler.step()
+    np.save(f"{out_dir}/full_accuracy.npy", np.array(test_acc))
+    np.save(f"{out_dir}/full_norm_weights_1.npy", np.array(model_norm1))
+    np.save(f"{out_dir}/full_norm_weights_2.npy", np.array(model_norm2))
 
     # ==================================================
-    fig, ax = plt.subplots()
-    ax.plot(test_acc)
-    ax.set_xlabel("epoch")
-    ax.set_ylabel('accuracy')
-    fig.savefig(f"{out_dir}/accuracy.svg", bbox_inches="tight")
-    plt.close(fig)
+    #   TRAINING WITH DROPOUT
+    n_epochs = 100
+    drop_p = 0.5
+    lr = 5e-3
+    wd = 1e-4
 
-    fig, ax = plt.subplots()
-    ax.plot(model_norm1, label='|w1|')
-    ax.plot(model_norm2, label='|w2|')
-    ax.legend()
-    ax.set_xlabel("epoch")
-    ax.set_ylabel('L2 weight norm')
-    fig.savefig(f"{out_dir}/weight_norm.svg", bbox_inches="tight")
-    plt.close(fig)
+    test_acc_p = []
+    model_norm1_p = []
+    model_norm2_p = []
+
+    model = Net(N, functools.partial(LinearExampleDropout, drop_p=drop_p)).to(device)
+    optimizer = optim.SGD(model.parameters(), lr=lr, weight_decay=wd)
+    scheduler = CosineAnnealingLR(optimizer, n_epochs)
+
+    print(model)
+
+    for epoch in range(1, n_epochs + 1):
+        train(model, device, train_loader, optimizer, epoch, log_interval=1000)
+        acc, weight_norm1, weight_norm2 = test(model, device, test_loader)
+        test_acc_p.append(acc)
+        model_norm1_p.append(weight_norm1)
+        model_norm2_p.append(weight_norm2)
+        scheduler.step()
+    np.save(f"{out_dir}/drop_accuracy.npy", np.array(test_acc_p))
+    np.save(f"{out_dir}/drop_norm_weights_1.npy", np.array(model_norm1_p))
+    np.save(f"{out_dir}/drop_norm_weights_2.npy", np.array(model_norm2_p))
+
+    # ==================================================
+    #      PLOTS
+    
+    test_acc = np.load(f"{out_dir}/full_accuracy.npy")
+    test_acc_p = np.load(f"{out_dir}/drop_accuracy.npy")
+    model_norm1_p = np.load(f"{out_dir}/drop_norm_weights_1.npy")
+    model_norm2_p = np.load(f"{out_dir}/drop_norm_weights_2.npy")
+    model_norm1 = np.load(f"{out_dir}/full_norm_weights_1.npy")
+    model_norm2 = np.load(f"{out_dir}/full_norm_weights_2.npy")
+
+    plt.figure(figsize=(10, 5))
+    plt.plot(test_acc, label='standard')
+    plt.plot(test_acc_p, label='p={}'.format(drop_p))
+    plt.legend()
+    plt.title('accuracy')
+    plt.savefig(f'{out_dir}/plot_accuracy.png', bbox_inches="tight")
+
+    plt.figure(figsize=(10, 5))
+    plt.plot(model_norm1, label='standard')
+    plt.plot(model_norm1_p, label='p={}'.format(drop_p))
+    plt.legend()
+    plt.title('L2 weight norm (fc1)')
+    plt.savefig(f'{out_dir}/plot_L2_weight_norm_fc1.png', bbox_inches="tight")
+
+    plt.figure(figsize=(10, 5))
+    plt.plot(model_norm2, label='standard')
+    plt.plot(model_norm2_p, label='p={}'.format(drop_p))
+    plt.legend()
+    plt.title('L2 weight norm (fc2)')
+    plt.savefig(f'{out_dir}/plot_L2_weight_norm_fc2.png', bbox_inches="tight")
--- job_array_pars.py
+++ job_array_pars.py
@@ -1,21 +1,12 @@
 import numpy as np
 import sys
 
-s_vals = ["sqrt", "lin "]
-
-l_vals = ["full", "drop"]
-
 N_vals = [100, 1000]
 
 p_vals = [0.0, 0.01, 0.02, 0.05, 0.1, 0.2, 0.5]
 
 with open("job_array_pars.txt", "w") as f:
-    for l in l_vals:
-        for s in s_vals:
-            for N in N_vals:
-                if l == "full":
-                    f.write(f"{s}\t{l}\t{N} \t0.00\n")
-                else:
-                    for p in p_vals:
-                        f.write(f"{s}\t{l}\t{N} \t{p:.2f}\n")
+    for N in N_vals:
+        for p in p_vals:
+            f.write(f"{N} \t{p:.2f}\n")
 
