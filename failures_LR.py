b9862fe5a933533da20b4e9adb23a2a74b4d1aa5
berberto <alberto.pzt@gmail.com>
2022-11-16 18:25:17 +0000

    options to skip training / analysis

--- failures_LR.py
+++ failures_LR.py
@@ -35,6 +35,9 @@ def generate_data (N, n_samples, **kwargs):
 
 if __name__ == "__main__":
 
+    training = True
+    analysis = True
+
     # ==================================================
     #   SETUP PARAMETERS
 
@@ -84,78 +87,82 @@ if __name__ == "__main__":
     # ==================================================
     #   TRAINING/TESTING
 
-    train_loss = []
-    test_acc = []
-    weights_norm = []
-    hidden_var = []
-    model_weights = []
+    if training:
 
-    # model = Net(N, layer_type=nn.Linear, scaling=scaling, bias=False).to(device)
-    model = Net(N, layer_type=functools.partial(LinearWeightDropout, drop_p=drop_p), bias=False, 
-                scaling=scaling, drop_l=drop_l).to(device)
-    optimizer = optim.SGD(model.parameters(), lr=lr, weight_decay=wd)
+        train_loss = []
+        test_acc = []
+        weights_norm = []
+        hidden_var = []
+        model_weights = []
 
-    model.save(f"{out_dir}/model_init")
+        # model = Net(N, layer_type=nn.Linear, scaling=scaling, bias=False).to(device)
+        model = Net(N, layer_type=functools.partial(LinearWeightDropout, drop_p=drop_p), bias=False, 
+                    scaling=scaling, drop_l=drop_l).to(device)
+        optimizer = optim.SGD(model.parameters(), lr=lr, weight_decay=wd)
 
-    print(model)
+        model.save(f"{out_dir}/model_init")
 
-    for epoch in range(n_epochs + 1):
-        # train
-        train_loader = generate_data(N, n_train, **train_kwargs)
-        loss = train(model, device, train_loader, optimizer, epoch, log_interval=1000)
-        # test
-        test_loader = generate_data(N, n_test, **test_kwargs)
-        acc, model_weights_, hidden_var_ = test(model, device, test_loader)
-        # collect statistics
-        if epoch % n_skip == 0:
-            model_weights.append(model_weights_)
-        train_loss.append(loss)
-        test_acc.append(acc)
-        hidden_var.append(hidden_var_)
-    model.save(f"{out_dir}/model_trained")
-    np.save(f"{out_dir}/train_loss.npy", np.array(train_loss))
-    np.save(f"{out_dir}/test_loss.npy", np.array(test_acc))
-    np.save(f"{out_dir}/hidden_var.npy", np.array(hidden_var))
-    with open(f"{out_dir}/weights.pkl", "wb") as f:
-        pickle.dump(model_weights, f)
+        print(model)
+
+        for epoch in range(n_epochs + 1):
+            # train
+            train_loader = generate_data(N, n_train, **train_kwargs)
+            loss = train(model, device, train_loader, optimizer, epoch, log_interval=1000)
+            # test
+            test_loader = generate_data(N, n_test, **test_kwargs)
+            acc, model_weights_, hidden_var_ = test(model, device, test_loader)
+            # collect statistics
+            if epoch % n_skip == 0:
+                model_weights.append(model_weights_)
+            train_loss.append(loss)
+            test_acc.append(acc)
+            hidden_var.append(hidden_var_)
+        model.save(f"{out_dir}/model_trained")
+        np.save(f"{out_dir}/train_loss.npy", np.array(train_loss))
+        np.save(f"{out_dir}/test_loss.npy", np.array(test_acc))
+        np.save(f"{out_dir}/hidden_var.npy", np.array(hidden_var))
+        with open(f"{out_dir}/weights.pkl", "wb") as f:
+            pickle.dump(model_weights, f)
 
     # ==================================================
     #      ANALYSIS
 
-    # get weights and calculate norm
-    with open(f"{out_dir}/weights.pkl", "rb") as f:
-        model_weights_ = pickle.load(f)
-        weights_list = []
-        weights_norm = []
-        svd_list = []
-        for i in range(len(model_weights_[0])):
-            weights_list.append( np.array([np.squeeze(w[i]) for w in model_weights_]) )
-            weights_norm.append( np.array([np.linalg.norm(w[i]) for w in model_weights_]) )
-    
-    with open(f"{out_dir}/weights_norm.pkl", "wb") as f:
-        pickle.dump(weights_norm)
+    if analysis:
+        
+        # get weights and calculate norm
+        with open(f"{out_dir}/weights.pkl", "rb") as f:
+            model_weights_ = pickle.load(f)
+            weights_list = []
+            weights_norm = []
+            svd_list = []
+            for i in range(len(model_weights_[0])):
+                weights_list.append( np.array([np.squeeze(w[i]) for w in model_weights_]) )
+                weights_norm.append( np.array([np.linalg.norm(w[i]) for w in model_weights_]) )
+        
+        with open(f"{out_dir}/weights_norm.pkl", "wb") as f:
+            pickle.dump(weights_norm, f)
 
-    W1 = weights_list[0]; norm1 = weights_norm[0]
-    W2 = weights_list[1]; norm2 = weights_norm[1]
+        W1 = weights_list[0]; norm1 = weights_norm[0]
+        W2 = weights_list[1]; norm2 = weights_norm[1]
 
-    # singular value decomposition of W1 for all snapshots
-    U, S, V = np.linalg.svd(W1)
+        # singular value decomposition of W1 for all snapshots
+        U, S, V = np.linalg.svd(W1)
 
-    # select dominant modes
-    mode = np.argmax(S, axis=1)
-    eval1 = S[:, mode]
-    Rvec1 = U[:, mode]
-    Lvec1 = V[:, mode]
+        # select dominant modes
+        mode = np.argmax(S, axis=1)
+        eval1 = S[:, mode]
+        Rvec1 = U[:, mode]
+        Lvec1 = V[:, mode]
 
-    # calculate the participation ratio
-    PR = np.array([np.sum(s)**2/np.sum(s**2) for s in S])
+        # calculate the participation ratio
+        PR = np.array([np.sum(s)**2/np.sum(s**2) for s in S])
 
-    # save spectral properties for all stored snapshots
-    np.save(f"{out_dir}/PR.npy", PR)
-    np.save(f"{out_dir}/eigenvalues.npy", S)
-    np.save(f"{out_dir}/eval.npy", eval1)
-    np.save(f"{out_dir}/Levec.npy", Lvec1)
-    np.save(f"{out_dir}/Revec.npy", Rvec1)
+        # save spectral properties for all stored snapshots
+        np.save(f"{out_dir}/PR.npy", PR)
+        np.save(f"{out_dir}/eigenvalues.npy", S)
+        np.save(f"{out_dir}/eval.npy", eval1)
+        np.save(f"{out_dir}/Levec.npy", Lvec1)
+        np.save(f"{out_dir}/Revec.npy", Rvec1)
 
     # ==================================================
     #      PLOTS
