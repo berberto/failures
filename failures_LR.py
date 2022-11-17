fixed (?) averages over/within batches; saving whole hidden layer

failures_LR.py
@@ -64,18 +64,17 @@ if __name__ == "__main__":
     # ==================================================
     #   SETUP TRAINING
 
-    n_epochs = 1000
+    n_epochs = 10000
 
-    n_train = 1000000
+    n_train = 100000
     n_test = 1000
     n_skip = min(100, n_epochs) # epochs to skip when saving data
 
-    lr = 1e-4
+    lr = 1e-5
     wd = 0.
 
-    batch_size = n_train//100
-    train_kwargs = {'batch_size': batch_size}
-    test_kwargs = {'batch_size': batch_size}
+    train_kwargs = {'batch_size': n_train//100}
+    test_kwargs = {'batch_size': n_test}
     use_cuda = True
     cuda_kwargs = {'num_workers': 1,
                     'pin_memory': True,
@@ -92,7 +91,7 @@ if __name__ == "__main__":
         train_loss = []
         test_acc = []
         weights_norm = []
-        hidden_var = []
+        hidden = []
         model_weights = []
 
         # model = Net(N, layer_type=nn.Linear, scaling=scaling, bias=False).to(device)
@@ -110,19 +109,21 @@ if __name__ == "__main__":
             loss = train(model, device, train_loader, optimizer, epoch, log_interval=1000)
             # test
             test_loader = generate_data(N, n_test, **test_kwargs)
-            acc, model_weights_, hidden_var_ = test(model, device, test_loader)
-            # collect statistics
-            if epoch % n_skip == 0:
-                model_weights.append(model_weights_)
+            acc, model_weights_, hidden_ = test(model, device, test_loader)
+
             train_loss.append(loss)
             test_acc.append(acc)
-            hidden_var.append(hidden_var_)
-        model.save(f"{out_dir}/model_trained")
-        np.save(f"{out_dir}/train_loss.npy", np.array(train_loss))
-        np.save(f"{out_dir}/test_loss.npy", np.array(test_acc))
-        np.save(f"{out_dir}/hidden_var.npy", np.array(hidden_var))
-        with open(f"{out_dir}/weights.pkl", "wb") as f:
-            pickle.dump(model_weights, f)
+            # collect statistics
+            if epoch % n_skip == 0:
+                hidden.append(hidden_)
+                model_weights.append(model_weights_)
+                # save data
+                model.save(f"{out_dir}/model_trained")
+                np.save(f"{out_dir}/train_loss.npy", np.array(train_loss))
+                np.save(f"{out_dir}/test_loss.npy", np.array(test_acc))
+                np.save(f"{out_dir}/hidden.npy", np.array(hidden))
+                with open(f"{out_dir}/weights.pkl", "wb") as f:
+                    pickle.dump(model_weights, f)
 
     # ==================================================
     #      ANALYSIS
@@ -170,7 +171,7 @@ if __name__ == "__main__":
     # re-load saved data
     train_loss = np.load(f"{out_dir}/train_loss.npy")
     test_acc = np.load(f"{out_dir}/test_loss.npy")
-    hidden_var = np.load(f"{out_dir}/hidden_var.npy")
+    hidden = np.load(f"{out_dir}/hidden.npy")
     with open(f"{out_dir}/weights_norm.pkl", "rb") as f:
         weights_norm = pickle.load(f)
 
@@ -212,7 +213,7 @@ if __name__ == "__main__":
     ax.set_xlabel('epoch')
     ax.set_ylim([0,1])
     for i, (norm, c) in enumerate(zip(weights_norm, colors)):
-        ax.plot(norm/norm[0], c=c, label=f'layer {i+1}')
+        ax.plot(norm/norm[0], c=c, label=f'{i+1}: {norm[0]}')
     ax.legend(loc='best')
     fig.savefig(f'{out_dir}/plot_weights_norm.png', bbox_inches="tight")
 
@@ -221,6 +222,6 @@ if __name__ == "__main__":
     ax.set_ylabel('Hidden layer variance')
     ax.set_xlabel('epoch')
     ax.set_ylim([0,1])
-    ln = ax.plot(hidden_var/hidden_var[0])
+    ln = ax.plot(np.linalg.norm(hidden, axis=1))
     fig.savefig(f'{out_dir}/plot_hidden_layer_variance.png', bbox_inches="tight")
 

training_utils.py
@@ -13,7 +13,7 @@ import os
 
 def train(model, device, train_loader, optimizer, epoch, log_interval=100, verbose=False):
     model.train()
-    loss = 0.
+    train_loss = 0.
     for batch_idx, (data, target) in enumerate(train_loader):
         data = data.clone().detach().float().to(device)
         target = target.clone().detach().float().view(-1,1).to(device)
@@ -21,40 +21,30 @@ def train(model, device, train_loader, optimizer, epoch, log_interval=100, verbo
         output = model(data)
         # "sum" or "mean" refers to the sum/average over both batch AND dimension indices
         # e.g. for a batch size of 64 and 10 classes, it is a sum/average of 640 numbers
-        loss += F.mse_loss(output, target, reduction="mean")/len(train_loader)
-    if epoch > 0:
-        loss.backward()
-        optimizer.step()
-        # if verbose:
-        #     if batch_idx % log_interval == 0:
-        #         print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-        #             epoch, batch_idx * len(data), len(train_loader.dataset),
-        #             100. * batch_idx / len(train_loader), loss.item()))
-    return loss.item()
+        loss = F.mse_loss(output, target, reduction="mean")
+        if epoch > 0:
+            loss.backward()
+            optimizer.step()
+        if verbose:
+            if batch_idx % log_interval == 0:
+                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                    epoch, batch_idx * len(data), len(train_loader.dataset),
+                    100. * batch_idx / len(train_loader), loss.item()))
+        train_loss += loss.item() / len(train_loader)
+    return train_loader
 
 def test(model, device, test_loader):
     model.eval()
     test_loss = 0
-    correct = 0
-    hidden_sq = 0
-    hidden_mean = 0 
+    hidden = 0
     with torch.no_grad():
         for data, target in test_loader:
             data = data.clone().detach().float().to(device)
             target = target.clone().detach().float().view(-1,1).to(device)
-            output, hidden = model.forward(data, hidden_layer=True)
-            test_loss += F.mse_loss(output, target, reduction="sum").item()# F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
-            hidden = hidden.detach().cpu().numpy()
-            hidden_mean += np.mean(hidden, axis=0)
-            hidden_sq += np.mean(hidden**2, axis=0)
-
-            # pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-            # correct += pred.eq(target.view_as(pred)).sum().item()
-
-    hidden_var = (hidden_sq - hidden_mean**2)/len(test_loader.dataset)
-    hidden_var = np.mean(hidden_var)
-
-    test_loss /= len(test_loader.dataset)
+            output, hidden_ = model.forward(data, hidden_layer=True)
+            hidden_ = np.mean(hidden_.detach().cpu().numpy(), axis=0)
+            hidden += hidden_/len(test_loader)
+            test_loss += F.mse_loss(output, target, reduction="mean").item() / len(test_loader)
 
     print('Test set: Average loss: {:.4f}'.format(test_loss))
     with torch.no_grad():
@@ -62,5 +52,5 @@ def test(model, device, test_loader):
         for name, pars in model.named_parameters():
             if "weight" in name:
                 model_weights.append(pars.detach().cpu().numpy())
-    return test_loss, model_weights, hidden_var
+    return test_loss, model_weights, hidden
 