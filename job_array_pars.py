0ea57c180ed7a9ab377bf8bb31c3011c4dd41b8c
berberto <alberto.pzt@gmail.com>
2023-01-22 19:48:39 +0000

    minor edits job submission

--- job_array_pars.py
+++ job_array_pars.py
@@ -3,11 +3,11 @@ import sys
 
 s_vals = ["sqrt"]
 
-N_vals = [100]
+N_vals = [784]
 
 p_vals = [0.1, 0.5] # [0.1, 0.25, 0.50, 0.75]
 
-d_vals = ["1", "12", "123"] # ["1", "12"]
+l_vals = ["1", "12", "123"]
 
 with open("job_array_pars.txt", "w") as f:
     for s in s_vals:
@@ -16,5 +16,5 @@ with open("job_array_pars.txt", "w") as f:
             f.write(f"{s}\t{N} \t0.00\t0\n")
             for p in p_vals:
                 # if dropout, chose option for layers
-                for d in d_vals:
-                    f.write(f"{s}\t{N} \t{p:.2f}\t{d}\n")
+                for l in l_vals:
+                    f.write(f"{s}\t{N} \t{p:.2f}\t{l}\n")
