b5d15622f602419f43294198875c93d32df5bd42
berberto <alberto.pzt@gmail.com>
2023-10-12 16:06:03 +0100

    [data] Saxe PNAS dataset

--- data.py
+++ data.py
@@ -32,6 +32,55 @@ class LinearRegressionDataset(torch.utils.data.Dataset):
         return self.X[i], self.y[i]
 
 
+class SemanticsDataset (torch.utils.data.Dataset):
+
+    # 0 - grow
+    # 1 - move
+    # 2 - roots
+    # 3 - fly
+    # 4 - swims
+    # 5 - leaves
+    # 6 - petals
+    N = 7
+
+    def __init__ (self, n_samples, cov=None):
+
+        # input-output covariance matrix
+        IO_cov = np.array([
+                [1,1,0,1,0,0,0],
+                [1,1,0,0,1,0,0],
+                [1,0,1,0,0,1,0],
+                [1,0,1,0,0,0,1]
+            ]).astype(float).T
+
+        # input-input covariance matrix
+        if cov is None:
+            cov = np.eye(self.N)
+
+        # target input-output map
+        self.w = np.dot( np.linalg.inv(cov), IO_cov ).T
+
+        X = np.random.multivariate_normal(np.zeros(self.N), cov, size=n_samples)
+        y = np.matmul(X, self.w.T)
+
+        self.X = torch.from_numpy(X)
+        self.y = torch.from_numpy(y)
+
+    @property
+    def data(self):
+        return self.X
+
+    @property
+    def targets(self):
+        return self.y
+
+    def __len__ (self):
+        return len(self.X)
+
+    def __getitem__ (self, i):
+        return self.X[i], self.y[i]
+
+
 if __name__ == "__main__":
     data = LinearRegressionDataset(np.ones(100), 10000)
 
